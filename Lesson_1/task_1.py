f4fd7e8521abd3e4ebba1b3dec02e2c4d23f86f5
Pavel <p.rodionov88@icloud.com>
2022-02-18 15:34:30 +0300

    The solution of the first task was added

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -1,1 +1,19 @@
-print(5*5)
+print(
+    'Поработайте с переменными, создайте несколько, выведите на экран. Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.')
+a = 10
+b = 45.23
+c = a + b
+d = a - b
+x = c / d
+print(a)
+print(b)
+print(c)
+print(d)
+print(x)
+
+name = input('Please, enter your name: ')
+print("Hi, %s!" % name)
+i = int(input(name + ' please, enter a random number: '))
+a = i + i * i
+print('Thank you very much. The answer is:')
+print(a)
