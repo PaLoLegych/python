4650e4ca0e49c50e04bcf301734896154f43a630
Pavel <p.rodionov88@icloud.com>
2022-02-24 16:36:05 +0300

    Added some changes.

--- Lesson_2/task_5.py
+++ Lesson_2/task_5.py
@@ -8,27 +8,21 @@
 # Пользователь ввёл число 1. Результат: 7, 5, 3, 3, 2, 1.
 # Набор натуральных чисел можно задать сразу в коде, например, my_list = [7, 5, 3, 3, 2].
 
-num = int(input('Please enter a number: '))
+num = int(input('Please enter a number: ')) #(enter 000 to Exit): '))
 my_list = [8, 8, 7, 5, 3, 3, 2]
 value = my_list.count(num)
+# while num != 000:
 for el in my_list:
     if value > 0:
         i = my_list.index(num)
         my_list.insert(i + 1, num)
-    # break
-    else:
-        if num > el:
-            j = my_list.index(el)
-            my_list.insert(j, num)
-            # break
-        elif num < my_list[len(my_list) - 1]:
-            my_list.append(num)
-
-    exit = input('Is the value inout complete? (Y/N) ')
-    if exit == 'y' or exit == 'Y':
         break
-    else:
-        if exit == 'n' or exit == 'N':
-            num = int(input('Please enter a number: '))
-
+    elif num > el:
+              j = my_list.index(el)
+              my_list.insert(j, num)
+#              break
+    elif num < my_list[len(my_list) - 1]:
+        my_list.append(num)
+#              break
 print(my_list)
+#num = int(input('Please enter a number: '))
