c52f872fa66adad31dc26fe3c3794df7aee5cf40
Pavel <p.rodionov88@icloud.com>
2022-02-27 19:46:25 +0300

    Something wrong (have to thing)

---
+++ Lesson_3/task_1.py
@@ -0,0 +1,16 @@
+#Реализовать функцию, принимающую два числа (позиционные аргументы)
+#и выполняющую их деление. Числа запрашивать у пользователя,
+# предусмотреть обработку ситуации деления на ноль.
+
+def my_func(arg_1, arg_2):
+
+    try:
+        arg_1 = int(input('Please enter a number greater than 0: '))
+        arg_2 = int(input('Please enter a divider (not 0): '))
+        result = arg_1 / arg_2
+    except ValueError:
+        return 'Wrong value. Try again. Value should be a digit'
+    except ZeroDivisionError:
+        return 'Incorrect divider! Divider cannot be 0'
+
+    return result
