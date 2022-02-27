48d9816440e73bcde627ff37ccbddfd8d1c44b28
Pavel <p.rodionov88@icloud.com>
2022-02-27 22:09:03 +0300

    more or less works.

--- Lesson_3/task_2.py
+++ Lesson_3/task_2.py
@@ -3,16 +3,13 @@
 # телефон. Функция должна принимать параметры как именованные аргументы.
 # Осуществить вывод данных о пользователе одной строкой.
 
-name = input('Please enter your name: ')
-surname = input('Please enter your surname: ')
-birthday = int(input('Please enter your birth year: '))
-location = input('Please enter your city of residence: ')
-email = input('Please enter your email address: ')
-phone = int(input('Please enter your phone number: '))
-
-
 def mfunc(name, surname, birthday, location, email, phone):
     return ' '.join([name, surname, birthday, location, email, phone])
 
 
-print(mfunc(name=' ', surname=' ', birthday=' ', location=' ', email=' ', phone=' '))
+print(mfunc(name=input('Please enter your name: '),
+            surname=input('Please enter your surname: '),
+            birthday=input('Please enter your birth year: '),
+            location=input('Please enter your city of residence: '),
+            email=input('Please enter your email address: '),
+            phone=input('Please enter your phone number: ')))
