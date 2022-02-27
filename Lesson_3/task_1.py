b4554953fab4b204c8025aed10735ccd3a3d6d23
Pavel <p.rodionov88@icloud.com>
2022-02-27 20:00:17 +0300

    Added some edits.

--- Lesson_3/task_1.py
+++ Lesson_3/task_1.py
@@ -1,9 +1,8 @@
-#Реализовать функцию, принимающую два числа (позиционные аргументы)
-#и выполняющую их деление. Числа запрашивать у пользователя,
+# Реализовать функцию, принимающую два числа (позиционные аргументы)
+# и выполняющую их деление. Числа запрашивать у пользователя,
 # предусмотреть обработку ситуации деления на ноль.
 
 def my_div(*args):
-
     try:
         arg_1 = int(input('Please enter a number (greater than 0): '))
         arg_2 = int(input('Please enter a divider (not 0): '))
@@ -14,5 +13,6 @@ def my_div(*args):
         return 'Incorrect divider! Divider cannot be 0'
 
     return result
+
 
 print(f'result {my_div()}')
