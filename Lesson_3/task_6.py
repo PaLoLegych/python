f995c5f76aa94e99f9476260f1565b1671b95a60
Pavel <p.rodionov88@icloud.com>
2022-03-01 17:22:03 +0300

    Completed.

---
+++ Lesson_3/task_6.py
@@ -0,0 +1,12 @@
+# Реализовать функцию int_func(), принимающую слова из маленьких латинских букв
+# и возвращающую их же, но с прописной первой буквой.
+# Например, print(int_func(‘text’)) -> Text.
+
+def mfunc(*args):
+    mtext = input('Please enter a short text: ')
+    print(mtext.title())
+    return
+
+
+mfunc()
+
