6a3c975241ff4934f5280cf4d0185668c6f570fc
Pavel <p.rodionov88@icloud.com>
2022-03-04 12:34:29 +0300

    Так и не понял почему не получается запустить через консоль. Буду дальше разбираться.

--- Lesson_4/task_1.py
+++ Lesson_4/task_1.py
@@ -3,15 +3,22 @@
 # (выработка в часах*ставка в час) + премия. Во время выполнения расчёта
 # для конкретных значений необходимо запускать скрипт с параметрами.
 
-def salary():
-    try:
-        hours = int(input('Please enter hours worked: '))
-        hrate = int(input('Please enter hourly rate: '))
-        bonus = int(input('Please enter bonus: '))
-        result = (hours * hrate) + bonus
-        print(f'Employee salary is: {result}')
-    except ValueError:
-        return print('Error. You must enter a numeric value!')
+# from sys import argv
+#
+# file_name, hours, hrate, bonus = argv
+#
+# salary = (float(hours)*float(hrate))+float(bonus)
+# print(f'Employee salary is: {salary}')
+
+ def salary():
+     try:
+         hours = int(input('Please enter hours worked: '))
+         hrate = int(input('Please enter hourly rate: '))
+         bonus = int(input('Please enter bonus: '))
+         result = (hours * hrate) + bonus
+         print(f'Employee salary is: {result}')
+     except ValueError:
+         return print('Error. You must enter a numeric value!')
 
 
-salary()
+ salary()
