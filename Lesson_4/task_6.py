dedfd68828b13ae744565cf12d02641fbcfac4d2
Pavel <p.rodionov88@icloud.com>
2022-03-04 13:00:22 +0300

    Completed.

--- Lesson_4/task_6.py
+++ Lesson_4/task_6.py
@@ -5,3 +5,27 @@
 # Предусмотрите условие его завершения. Например, в первом задании выводим целые числа, начиная с 3.
 # При достижении числа 10 — завершаем цикл. Вторым пунктом необходимо предусмотреть условие,
 # при котором повторение элементов списка прекратится.
+
+from itertools import count, cycle
+
+
+def count_func(start_num, stop_num):
+    for i in count(start_num):
+        if i > stop_num:
+            break
+        else:
+            print(i)
+
+
+def cycle_func(mlist, output):
+    i = 0
+    output_num = cycle(mlist)
+    while i < output:
+        print(next(output_num))
+        i += 1
+
+
+count_func(start_num=int(input('Please enter start value: ')),
+           stop_num=int(input('Please enter stop value: ')))
+cycle_func(mlist=[4, 10, 44, 7, 23, 99, 152, 3.14, 6.022, True, 'ABC', 'abc'],
+           output=int(input('Please enter the number of parameters to display: ')))
