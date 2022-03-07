3d98b3399357622c75e651ee20082c71f3e25f5d
Pavel <p.rodionov88@icloud.com>
2022-03-07 14:14:13 +0300

    Something wrong. Still can't find the problem.

---
+++ Lesson_5/task_4.py
@@ -0,0 +1,31 @@
+# Создать (не программно) текстовый файл со следующим содержимым:
+# One — 1
+# Two — 2
+# Three — 3
+# Four — 4
+# Напишите программу, открывающую файл на чтение и считывающую
+# построчно данные. При этом английские числительные должны
+# заменяться на русские. Новый блок строк должен
+# записываться в новый текстовый файл.
+
+# from googletrans import Translator
+#
+# with open('text_4.txt', 'r', encoding='utf-8') as f:
+#     if f.mode == 'r':
+#         content = f.read()
+#         print(content)
+#
+#     translator = Translator()
+#     result = translator.translate(content, dest='ru')
+#     print(result.text)
+
+mdict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+new_list = []
+with open('text_4.txt', 'r') as f:
+    for i in f:
+        i = i.split('-', 1)
+        new_list.append(mdict[i[0]] + ' ' + i[1])
+    print(new_list)
+
+with open('text_4_1.txt', 'w') as f_new:
+     f_new.writelines(new_list)
