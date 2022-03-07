7527a0372a2b0304d524c259ebe494345e7f8101
Pavel <p.rodionov88@icloud.com>
2022-03-07 14:50:13 +0300

    Something wrong. Three different attempts and still can't figure out what's wrong

--- Lesson_5/task_4.py
+++ Lesson_5/task_4.py
@@ -20,12 +20,23 @@
 #     print(result.text)
 
 mdict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+
 new_list = []
-with open('text_4.txt', 'r') as f:
+with open('text_4.txt', 'r', encoding='utf-8') as f:
     for i in f:
         i = i.split('-', 1)
         new_list.append(mdict[i[0]] + ' ' + i[1])
     print(new_list)
 
 with open('text_4_1.txt', 'w') as f_new:
-     f_new.writelines(new_list)
+    f_new.writelines(new_list)
+
+# mdict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+#
+# new_list = []
+# with open('text_4.txt', 'r', encoding='utf-8') as f:
+#     with open('text_4_1.txt', 'w', encoding='utf-8') as f2:
+#         for line in f:
+#             el, *num = line.split()
+#             ru = mdict[el]
+#             f2.write(' '.join([ru] + num) + '\n')
