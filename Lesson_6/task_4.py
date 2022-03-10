ed623f4be8a61b1556baec45662799be647dcc27
Pavel <p.rodionov88@icloud.com>
2022-03-10 21:28:56 +0300

    In progress...

---
+++ Lesson_6/task_4.py
@@ -0,0 +1,51 @@
+# Реализуйте базовый класс Car.
+# у класса должны быть следующие атрибуты: speed, color, name, is_police (булево).
+# А также методы: go, stop, turn(direction), которые должны сообщать,
+# что машина поехала, остановилась, повернула (куда);
+# опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar;
+# добавьте в базовый класс метод show_speed, который должен показывать
+# текущую скорость автомобиля; для классов TownCar и WorkCar переопределите
+# метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar)
+# должно выводиться сообщение о превышении скорости.
+# Создайте экземпляры классов, передайте значения атрибутов.
+# Выполните доступ к атрибутам, выведите результат.
+# Вызовите методы и покажите результат.
+
+class Car:
+
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = bool(is_police)
+
+    def go(self)
+        pass
+
+    def stop(self):
+        pass
+
+    def turn(self):
+        pass
+
+
+class TownCar(Car):
+
+    def towncar(self):
+        pass
+
+class SportCar(Car):
+
+    def sportcar(self):
+        pass
+
+class WorkCar(Car):
+
+    def workcar(self):
+        pass
+
+
+class PoliceCar(Car):
+
+    def policecar(self):
+        pass
