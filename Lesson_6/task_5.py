db317bbb1ebe2a5e9dc89631f72d503c4ad5dfb0
Pavel <p.rodionov88@icloud.com>
2022-03-11 12:34:14 +0300

    In the process...

---
+++ Lesson_6/task_5.py
@@ -0,0 +1,30 @@
+# Реализовать класс Stationery (канцелярская принадлежность).
+# определить в нём атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение «Запуск отрисовки»;
+# создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
+# в каждом классе реализовать переопределение метода draw.
+# Для каждого класса метод должен выводить уникальное сообщение;
+# создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Start the rendering process.')
+
+
+class Pen(Stationary):
+    pass
+
+
+class Pencil(Stationary):
+    pass
+
+
+class Handle(Stationary):
+    pass
+
+
+a = Stationary("Beginning")
+a.draw()
+print(a.draw())
