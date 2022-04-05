3b131e32c131de4635f660b10d74297a8955ae8c
Pavel <p.rodionov88@icloud.com>
2022-04-05 17:15:04 +0300

    Almost done. Need to improve.

---
+++ Lesson_8/task_3.py
@@ -0,0 +1,25 @@
+#  Создайте собственный класс-исключение, который должен проверять содержимое списка на наличие только чисел.
+#  Проверить работу исключения на реальном примере. Запрашивать у пользователя данные и заполнять список
+#  необходимо только числами. Класс-исключение должен контролировать типы данных элементов списка.
+
+class MyOwnErr:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def content(self):
+        while True:
+            try:
+               value = int(input('Please enter a number: '))
+               self.my_list.append(value)
+               print(f'The current list is: {self.my_list} \n')
+            except:
+                print('This value is not allowed!')
+                choice = input('Would you like to try again? (Y/N)? - ')
+
+                if choice == 'Y' or choice == 'y':
+                    print(my_exception.content())
+                elif choice == 'N' or choice == 'n':
+                    print('Exit done.')
+
+my_exception = MyOwnErr(1)
+print(my_exception.content())
